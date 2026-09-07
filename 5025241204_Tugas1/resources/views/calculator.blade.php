7d63254f60d27f9df61f6279e9f0cd37314bb9f9
Lilium <scientificwitches@gmail.com>
2026-09-07 14:47:22 +0700

    Add files via upload

---
+++ 5025241204_Tugas1/resources/views/calculator.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Calculator')
+
+@section('content')
+
+<img src="{{ asset('images/bg-campus.png') }}" class="absolute inset-0 w-full h-full object-cover"></div>
+<section class="relative min-h-screen flex items-center justify-center">
+    {{-- Calculator UI goes here later --}}
+</section>
+@endsection
